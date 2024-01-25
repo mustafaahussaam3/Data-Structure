94fd353b4a4d1478eeca8f80c8087789b8e642d1
mustafaahussaam3 <mustafahussameldin@gmail.com>
2024-01-25 13:53:49 +0200

    OOP Program

---
+++ 04OOP.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+
+private:
+    int length;
+    int breadth;
+
+public:
+
+    Rectangle (int l, int b) : length(l), breadth(b){}
+
+    int area (){
+
+        return length * breadth;
+    }
+
+    int peri(){
+        
+        return 2*(length + breadth);
+    }
+
+};
+int main(){
+
+    int l,b;
+    printf("Enter Length and Breadth");
+
+    cin>>l>>b;
+    Rectangle r(l,b);
+
+    int ar = r.area();
+    int pr = r.peri();
+
+    printf("Area %d \nPerimeter %d \n",ar, pr);
+
+
+    return 0;
+}
