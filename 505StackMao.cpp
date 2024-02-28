490df8599900c610f70d7cdcd0d9bfc0cb81ed14
mustafaahussaam3 <mustafahussameldin@gmail.com>
2024-02-28 15:16:00 +0200

    Stack and Map

---
+++ 505StackMao.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+
+string isBalanced(string s)
+{
+
+    map <char, char> mapping;
+    mapping['}'] = '{' ;
+    mapping[')'] = '(' ;
+    mapping[']'] = '[' ;
+
+    map <char, char>::iterator it;
+    stack <char> st;
+    int count = 0;
+    for ( int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '{' || s[i] == '(' || s[i] == '[')
+        {
+            st.push (s[i]);
+            count ++ ;
+        }
+        else if (s[i] == '}' || s[i] == ')' || s[i] == ']')
+        {
+            if (st.empty())
+                return "0";
+            else 
+            {
+                char temp  = st.top();
+                it = mapping.find(s[i]);
+                if (temp = it->second)
+                {
+                    st.pop();
+                }
+                else 
+                    return "0";
+            }
+        }
+    }
+    stringstream ss;
+    ss << 1 << " " << count;
+    return st.empty() ? ss.str() : "0" ;
+
+}
+int main ()
+{
+    string A = "{([a+b]*[c-d])/e}";
+    cout << isBalanced(A) << endl;
+ 
+    string B = "{([a+b]}*[c-d])/e}";
+    cout << isBalanced(B) << endl;
+ 
+    string C = "{([{a+b]*[c-d])/e}";
+    cout << isBalanced(C) << endl;
+
+    return 0;
+}
--- try.cpp
+++ try.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+
 int main ()
-{
-    string s = {"12, 2, 3, 4, 5"};
-    int n = stoi(s.substr(0, s.find(',',0) - 0));
-    string s1 = s.substr(2,1);
-    cout << "string" << s1 << endl ;
-    cout << n << endl ;
+{   
+    map<int, int> mp;
+    map<int, int>::iterator it;
+
+    mp[1] = 2;
+    mp[2] = 4;
+    mp[3] = 2;
+    mp[1] = 100;
+    mp[2] = 200;
+    mp[3] = 100;
+
+    for (it = mp.begin (); it != mp.end(); it++)
+    {   
+        cout << it->first << " " << it->second << endl;
+    }
     return 0;
 }
+
+    // string s = {"12, 2, 3, 4, 5"};
+    // int n = stoi(s.substr(0, s.find(',',0) - 0));
+    // string s1 = s.substr(2,1);
+    // cout << "string" << s1 << endl ;
+    // cout << n << endl ;
